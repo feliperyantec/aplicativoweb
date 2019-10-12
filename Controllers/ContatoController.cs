f1af6c663824854c160264fabe19a641aa03b02c
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-10-11 20:51:40 -0400

    Aplicativo Web

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicativoWeb.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
